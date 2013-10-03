1c1fef8b3ccb32ba97f793cadbbf85e207aa18eb
Ronan Le Bris <ronan.lebris.rolebi@gmail.com>
2013-10-03 15:33:32 +0200

    add missing license informations

--- UnsafeDependenciesException.php
+++ UnsafeDependenciesException.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+* This file is part of the rolebi/ComposerDependenciesSecurityChecker.
+*
+* (c) 2013 Ronan Le Bris
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
+
 namespace Rolebi\ComposerDependenciesSecurityChecker;
 
 use SensioLabs\Security\SecurityChecker;
